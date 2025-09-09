9c62b387e60f23bda8a5ee1f20f304fb0a4a23e1
Awanda <awandapolman@gmail.com>
2025-09-09 20:43:06 +0800

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,40 @@
+package day2;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+       
+        System.out.print("Halo ");
+        System.out.print("wanda!");
+    }
+}
+
+package day2;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+       
+        System.out.println("Halo");
+        System.out.println("wanda");
+    }
+}
+
+package day2;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        // Contoh variabel
+        int umur = 21;
+        double nilai = 88.00;
+        char inisial = 'A';
+        boolean lulus = true;
+
+        // Menggunakan printf()
+        System.out.printf("Umur : %d tahun\n", umur);        // %d untuk int
+        System.out.printf("Nilai uas: %.2f\n", nilai);         // %.2f untuk double (2 angka di belakang koma)
+        System.out.printf("inisial: %c\n", inisial);                 // %c untuk char
+        System.out.printf("Apakah lulus? %b\n", lulus);          // %b untuk boolean
+    }
+}l
