6689a2749cd2198edfaddd46f64c5dc9d1af5db0
Awanda <awandapolman@gmail.com>
2025-09-10 21:36:07 +0800

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,16 @@
+package day3;
+
+public class Day3 {
+    public static void main(String[] args) {
+  // Komentar satu baris 
+        // Komentar ini hanya berlaku pada satu baris saja.
+        System.out.println("Halo, ini contoh komentar satu baris.");
+
+        /* Komentar banyak baris 
+           Komentar ini bisa ditulis lebih dari satu baris.
+           Cocok untuk menjelaskan kode yang agak panjang.
+        */
+        System.out.println("Ini contoh komentar banyak baris.");
+    }
+    
+} 
